c3fc63dbce4ead34c7fbcd854cc0e5e17833d79a
vARMORv <116925504+vARMORv@users.noreply.github.com>
2022-11-21 00:37:14 +0200

    Добавьте файлы проекта.

---
+++ noHealthFlick/dllmain.cpp
@@ -0,0 +1,17 @@
+﻿#include "main.h"
+
+template <typename T>
+inline void write_memory(std::uintptr_t address, T value, bool protect = true) {
+    unsigned long oldProt;
+    if (protect) VirtualProtect(reinterpret_cast<void*>(address), sizeof(T), PAGE_READWRITE, &oldProt);
+    *reinterpret_cast<T*>(address) = value;
+    if (protect) VirtualProtect(reinterpret_cast<void*>(address), sizeof(T), oldProt, &oldProt);
+}
+
+AsiPlugin::AsiPlugin() {
+    write_memory(0x5892AB, char(0), true);
+}
+
+AsiPlugin::~AsiPlugin() {
+    write_memory(0x5892AB, char(10), true);
+}
---
+++ noHealthFlick/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <windows.h>
+#include <memory>
+
+class AsiPlugin {
+
+public:
+    explicit AsiPlugin();
+    virtual ~AsiPlugin();
+} AsiPlugin;
+
+#endif
